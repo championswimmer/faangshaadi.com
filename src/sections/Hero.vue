12b9e97a2e6fdddbbe4b2891cdb9b3292fdf0bcf
Arnav Gupta <arnav@codingblocks.com>
2021-03-28 18:25:25 +0530

    basic template of website kind of ready
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

--- src/sections/Hero.vue
+++ src/sections/Hero.vue
@@ -1,15 +1,15 @@
 <template>
-    <div class="row p-4 p-md-5 mb-4 text-white bg-dark">
+    <div class="row p-4 p-md-5 mb-4">
         <div class="col">
             <h1 class="display-4 fst-italic">Title of a longer featured blog post</h1>
             <p
                 class="lead my-3"
             >Multiple lines of text that form the lede, informing new readers quickly and efficiently about what’s most interesting in this post’s contents.</p>
             <p class="lead mb-0">
-                <a href="#" class="text-white fw-bold">Continue reading...</a>
+                <a href="#" class="font-monospace fw-bold">Continue reading...</a>
             </p>
         </div>
-        <div class="col">
+        <div class="col p-4">
             <img src="@/assets/illustrations/holding-hands.svg">
         </div>
     </div>
--- src/sections/NavBar.vue
+++ src/sections/NavBar.vue
@@ -1,7 +1,7 @@
 <template>
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         <div class="container-fluid">
-          <a class="navbar-brand" href="#">FAANGShaadi.com</a>
+          <a class="navbar-brand font-display" href="#">FAANGShaadi.com</a>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
